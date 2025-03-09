62325c59dc29438324389de0306e01d90f9383b3
Sargis <mnoyansargis@gmail.com>
2025-03-10 00:08:30 +0400

    Uxxaki urls.py um sxal er grac

--- main/urls.py
+++ main/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('login/',LoginPage,name='login'),
     path('logout/',Logout,name='logout'),
     #----------------------------------------------
-    path('home/',HomeListView.as_view(),name='home'),
+    path('',HomeListView.as_view(),name='home'),
     path('about/',AboutListView.as_view(),name='about'),
     path('pr_list/',PropertyListView.as_view(),name = 'pr_list'),
     path('contact/',ContactPage.as_view(),name = 'contact'),
